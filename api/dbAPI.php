66f7b85de31e4e8dfe8f78f05fbcc5e3b1e10754
Ajit Meesala <67087879+AjitMeesala@users.noreply.github.com>
2025-03-01 22:40:07 +0530

    Update dbAPI.php
    Security Patch

--- api/dbAPI.php
+++ api/dbAPI.php
@@ -1,25 +1,13 @@
 
 <?php
 $conn = mysqli_connect(
-    "103.21.58.5",
-    "stepcone",
-    "Hawking@1234",
-    "stepcone"
+    "{write ip address here}",
+    "db username",
+    "db Password",
+    "db name"
 );
 
 if (!$conn) {
     die("Connection Error : ".mysqli_connect_error());
 }
-
-// $host="102.21.58.5";
-// $port=3306;
-// $socket="";
-// $user="root";
-// $password="";
-// $dbname="stepcone2023";
-
-// $conn = new mysqli($host, $user, $password, $dbname, $port, $socket);
-// if (!$conn) {
-//     die ('Could not connect to the database server' . mysqli_connect_error());
-// }
 ?>
